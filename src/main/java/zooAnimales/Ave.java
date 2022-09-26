5df19f590aac3026257bfb16b5a069796f55e49f
russbelln <92810561+russbelln@users.noreply.github.com>
2022-09-26 11:55:48 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -12,7 +12,6 @@ public class Ave extends Animal{
 
     public Ave(){
         super();
-        Ave.listado.add(this);
     }
 
     public Ave( String nombre, int edad, String habitat, String genero, String colorPlumas) {
